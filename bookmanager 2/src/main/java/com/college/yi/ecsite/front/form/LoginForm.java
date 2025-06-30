cd02c4228338e70a5d2b6c294816bb374a1eed60
NobutoMyodo <M.Nobuto3527@gmail.com>
2025-06-30 09:57:32 +0900

    認証機能：ログインテストクラス、Login機能一部修正
    確認お願い致します。セキュリティコンフィグで行っていた処理をHTMLに置き換えました。

--- bookmanager 2/src/main/java/com/college/yi/ecsite/front/form/LoginForm.java
+++ bookmanager 2/src/main/java/com/college/yi/ecsite/front/form/LoginForm.java
@@ -1,16 +1,14 @@
 package com.college.yi.ecsite.front.form;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
 import lombok.Data;
 
 @Data
 public class LoginForm {
-    @NotBlank(message = "メールアドレスを入力してください")
-    @Email(message = "正しいメールアドレス形式で入力してください")
+	
+
     private String email;
 
-    @NotBlank(message = "パスワードを入力してください")
+
     private String password;
+    
 }
